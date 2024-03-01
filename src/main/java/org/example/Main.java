6ef8416677f66fb7a2dc6165eb2e88a65e137cc9
REDTECH <vashonmabima12@gmail.com>
2024-03-01 23:13:14 +0530

    Created 2 entities using hibernate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import org.example.config.FactoryConfiguration;
+import org.example.entity.Book;
+import org.example.entity.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        Book book = new Book();
+        book.setId("b001");
+        book.setTitle("Harry Potter");
+        book.setAuthor("Emma");
+        book.setGenre("adventure");
+        book.setAvailability_status("available");
+
+        User user = new User();
+        user.setId("u001");
+        user.setName("Veshon");
+        user.setAddress("UK");
+        user.setEmail("veshonmabima17@gmail.com");
+        user.setPassword("1234");
+
+        session.save(user);
+        session.save(book);
+
+        book.setUser(user);
+
+        transaction.commit();
+        session.close();
     }
 }
